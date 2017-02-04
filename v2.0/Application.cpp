84ae3f23134e68148727f68cf6b1fe1ea713ca35
Brian Gradin <brian@toolhouse.com>
2017-02-03 16:13:43 -0800

    Blueprinting

---
+++ v2.0/Application.cpp
@@ -0,0 +1,15 @@
+// Lifetime
+// - Initializes members
+
+// Inputs
+// (none)
+
+// Members
+// - MainWindow
+// - Keyboard
+// - GraphicsContext (?)
+// - Game
+// - Audio (?)
+
+// Methods
+// (none)
---
+++ v2.0/MainWindow.cpp
@@ -0,0 +1,13 @@
+// Lifetime
+// - Initializes members
+// - Sets up window
+
+// Inputs
+// (none)
+
+// Members
+// Window title
+
+// Methods
+// - SetTitle
+// pa CreateWindow
